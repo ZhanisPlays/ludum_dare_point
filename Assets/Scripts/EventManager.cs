fb7e161e895a85a894f971b1e39476dfdb254847
= <Strautins.Janis@gmail.com>
2018-04-20 21:51:20 +0300

    Even more initial stuff.

---
+++ Assets/Scripts/EventManager.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using Utility;
+
+public class Events {
+    public const string EVENT = "";
+}
+
+public class BaseEvent : UnityEvent<BaseMessage> {
+}
+
+public class BaseMessage {
+}
+
+public class StringMessage : BaseMessage {
+    public string value;
+
+    public StringMessage(string value) {
+        this.value = value;
+    }
+}
+
+public class BoolMessage : BaseMessage {
+    public bool value;
+
+    public BoolMessage(bool value) {
+        this.value = value;
+    }
+}
+
+public class EventManager : MonoBehaviour {
+
+    private Dictionary<string, BaseEvent> eventDictionary;
+    private static EventManager eventManager;
+
+    public void Awake() {
+        if (eventManager == null) {
+            eventManager = this;
+        } else if (eventManager != this) {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public static EventManager Instance {
+        get {
+            if (eventManager == null) {
+                eventManager = FindObjectOfType<EventManager>();
+                if (eventManager == null) {
+                    Utils.Log("EventManager: Object not found in the scene!", LogType.ERROR);
+                }
+                eventManager.Init();
+            }
+            return eventManager;
+        }
+    }
+
+    void Init() {
+        Utils.Log("EventManager: Init");
+        if (eventDictionary == null) {
+            eventDictionary = new Dictionary<string, BaseEvent>();
+        }
+    }
+
+    public static void StartListening(string eventName, UnityAction<BaseMessage> listener) {
+        BaseEvent thisEvent = null;
+        if (!Instance.eventDictionary.TryGetValue(eventName, out thisEvent)) {
+            thisEvent = new BaseEvent();
+            Instance.eventDictionary.Add(eventName, thisEvent);
+        }
+        thisEvent.AddListener(listener);
+    }
+
+    public static void StopListening(string eventName, UnityAction<BaseMessage> listener) {
+        if (eventManager != null) {
+            BaseEvent thisEvent = null;
+            if (Instance.eventDictionary.TryGetValue(eventName, out thisEvent)) {
+                thisEvent.RemoveListener(listener);
+            }
+        }
+    }
+
+    public static void TriggerEvent(string eventName, BaseMessage msg = null) {
+        BaseEvent thisEvent = null;
+        if (Instance.eventDictionary.TryGetValue(eventName, out thisEvent)) {
+            Utils.Log("EventManager: Triggering `" + eventName + "`");
+            thisEvent.Invoke(msg);
+        } else {
+            Utils.Log("EventManager: No listeners for:  `" + eventName + "`", LogType.WARNING);
+        }
+    }
+
+}
---
+++ Assets/Scripts/Utils.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum LogType {
+    INFO,
+    ERROR,
+    WARNING
+}
+namespace Utility {
+    public class Utils {
+        public static void DebugDictioanry<T, V>(Dictionary<T, V> dictionary) {
+            Utils.Log("\nSize: " + dictionary.Count);
+            foreach (KeyValuePair<T, V> item in dictionary) {
+                Utils.Log(string.Format("Key: {0}, Value: {1}", item.Key, item.Value));
+            }
+        }
+
+        public static void Log(object msg, LogType logType = LogType.INFO) {
+            Debug.Log(GetPrefix(logType) + msg.ToString() + GetPostFix());
+        }
+
+        private static string GetPrefix(LogType logType) {
+            string format = "<color={0}>";
+            string arg = "blue";
+            switch (logType) {
+                case LogType.ERROR:
+                    arg = "red";
+                    break;
+                case LogType.WARNING:
+                    arg = "yellow";
+                    break;
+                case LogType.INFO:
+                default:
+                    break;
+            }
+            return string.Format(format, arg);
+        }
+
+        private static string GetPostFix() {
+            return "</color>";
+        }
+    }
+}
